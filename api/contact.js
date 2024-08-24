21f8e34d4bae9128e110fd3ffae7ab66f7e113ca
me-abdullah <enzoemrick@gmail.com>
2024-08-24 22:41:45 +0500

    working

---
+++ api/contact.js
@@ -0,0 +1,34 @@
+// api/contact.js
+import nodemailer from 'nodemailer';
+
+export default async (req, res) => {
+  if (req.method === 'POST') {
+    const { name, email, phone_number, zipcode, message } = req.body;
+
+    let transporter = nodemailer.createTransport({
+      service: 'Gmail',
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+
+    let mailOptions = {
+      from: email,
+      to: 'epicsteamanddeepcleaning@gmail.com',
+      subject: 'New Form Submission',
+      text: `You have received a new message from your website form.\n\nFull Name: ${name}\nEmail: ${email}\nPhone: ${phone_number}\nZip Code: ${zipcode}\nMessage:\n${message}`,
+    };
+
+    try {
+      await transporter.sendMail(mailOptions);
+      res.status(200).json({ message: 'Message sent successfully!' });
+    } catch (error) {
+      console.error('Error sending email:', error);
+      res.status(500).json({ message: 'Failed to send message. Please try again later.' });
+    }
+  } else {
+    res.setHeader('Allow', ['POST']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+};
